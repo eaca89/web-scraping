ac6dbc61e502b0088287d88c0a8459f1aedd13d2
Ehsan <eaca89@gamil.com>
2022-01-10 01:39:00 +0330

    now, the code is mostly based on API

--- webScraper/results/views.py
+++ webScraper/results/views.py
@@ -1,25 +1,25 @@
-from django.shortcuts import render
-from typing import Counter
-import requests
-from bs4 import BeautifulSoup as bs
-from requests.api import head
 from collections import Counter
+from django.shortcuts import render
+from bs4 import BeautifulSoup as bs
+from datetime import datetime, timedelta
+import requests
 
 new_link_list = []
 top_link_list = []
 tag_list = []
+
 # new list
 def getNewList(keyword):
     new_list = []
-    myurl = 'https://stackoverflow.com/questions/tagged/'+str(keyword)+'?tab=Newest'+str(keyword)
-    r = requests.get(myurl)
-    new = bs(r.content, features="html.parser")
-    summary = new.find_all('div', attrs={'class': 'summary'})
+    url1 = 'https://api.stackexchange.com/2.3/questions?'
+    oneDayAgo = datetime.now() - timedelta(1)
+    url2 = 'fromdate='+str(oneDayAgo.date())+'&order=desc&sort=votes&'    
+    url3 = 'tagged='+str(keyword)+'&site=stackoverflow'
+    response = requests.get(url1 + url2 + url3).json()
     i = 0
-    # new_link_list = []
-    for item in summary:
-        new_list.append("{}- ".format(i+1) + item.h3.a.string)
-        new_link_list.append(item.a['href'])
+    for x in range(len(response['items'])):
+        new_list.append("{}- ".format(i+1) + response['items'][x]['title'])
+        new_link_list.append(response['items'][x]['link'])
         i += 1
         if (i == 10):
             break
@@ -27,45 +27,45 @@ def getNewList(keyword):
 
 # top list
 def getTopList(keyword):
-    top_list = []    
-    myurl = 'https://stackoverflow.com/questions/tagged/'+str(keyword)+'?tab=Votes'
-    r = requests.get(myurl)
-    top = bs(r.content, features="html.parser")
-    summary = top.find_all('div', attrs={'class': 'summary'})    
+    top_list = []
+    oneWeekAgo = datetime.now() - timedelta(7)
+    url1 = 'https://api.stackexchange.com/2.3/questions?'
+    url2 = 'fromdate='+str(oneWeekAgo.date())+'&order=desc&sort=votes&'    
+    url3 = 'tagged='+str(keyword)+'&site=stackoverflow'
+    response = requests.get(url1 + url2 + url3).json()    
     i = 0
-    # top_link_list = []
-    for item in summary:
-        top_list.append("{}- ".format(i+1) + item.h3.string)
-        top_link_list.append(item.a['href'])
-        # print(top_link_list[i])
+    for x in range(len(response['items'])):
+        top_list.append("{}- ".format(i+1) + response['items'][x]['title'])
+        top_link_list.append(response['items'][x]['link'])
         i += 1
         if (i == 10):
             break
+    
     # find all tags
-    tags = top.find_all('div', attrs={'class': 'tags'})
-    for tag in tags:
-        tag_list.append(tag.a.string)
-        
+    for i in range(len(response['items'])):
+        for item in response['items'][i]['tags']:
+            tag_list.append(item)
+    
     return top_list    
 
 def trends(tagList):
-
     cnt = Counter(tagList)
-    temp = cnt.most_common(4)
-    top_four = []
+    temp = cnt.most_common(5)
+    top_five = []
     for item in temp:
-        top_four.append('#'+str(item[0]))
+        top_five.append('#'+str(item[0]))
     # empty tag_list
     global tag_list
     tag_list = []
-    return top_four
+    return top_five
 
 def index(request):
     keyword = request.POST.get('keyword')
     
     # if the input is empty, return nothing
     if keyword == '':
-        return render(request, 'results/index.html')
+        return render(request, 'results/index.html')    
+        
     class post:
         def __init__(self, link, id):           
             self.link = link
@@ -79,12 +79,12 @@ def index(request):
     for item in lst1:        
         lst3.append(post(item, i))
         i += 1
+    
     i=0
-
     for item in lst2:        
         lst4.append(post(item, i))
         i += 1
-    print(tag_list)
+
     context = {
         'new_list': lst3,
         'top_list': lst4,
@@ -95,12 +95,14 @@ def index(request):
 
 def NewlistItem(request, id):
     context = {}
-    link = 'https://stackoverflow.com'+new_link_list[id]
+    link = new_link_list[id]
     r = requests.get(link)
     c = bs(r.content, features="html.parser")
+    title = c.find('div', attrs={'id': 'question-header'}).h1.get_text()
     Question = c.find('div', attrs={'class': 'question'}).prettify()
     Answer = c.find('div', attrs={'id': 'answers'}).prettify()    
     context = {
+        'title': title,
         'mainQ': Question,
         'thread': Answer,
         'link': link,
@@ -109,12 +111,14 @@ def NewlistItem(request, id):
 
 def ToplistItem(request, id):
     context = {}
-    link = 'https://stackoverflow.com'+top_link_list[id]
+    link = top_link_list[id]
     r = requests.get(link)
     c = bs(r.content, features="html.parser")
+    title = c.find('div', attrs={'id': 'question-header'}).h1.get_text()
     Question = c.find('div', attrs={'class': 'question'}).prettify()
     Answer = c.find('div', attrs={'id': 'answers'}).prettify()    
     context = {
+        'title': title,
         'mainQ': Question,
         'thread': Answer,
         'link': link,
