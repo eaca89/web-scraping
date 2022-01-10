d043ef15ec2c95519edb1c14ab99c36c15e675c7
Ehsan <eaca89@gamil.com>
2022-01-10 14:18:37 +0330

    some debugging and cleaning

--- webScraper/results/urls.py
+++ webScraper/results/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('NewlistItem/<int:id>/', views.NewlistItem, name='NewlistItem'),
-    path('ToplistItem/<int:id>/', views.ToplistItem, name='ToplistItem'),
+    path('NewlistItem/<int:id>/', views.new_list_item, name='NewlistItem'),
+    path('ToplistItem/<int:id>/', views.top_list_item, name='ToplistItem'),
 ]
--- webScraper/results/views.py
+++ webScraper/results/views.py
@@ -4,39 +4,69 @@ from bs4 import BeautifulSoup as bs
 from datetime import datetime, timedelta
 import requests
 
+new_title_list =[]
 new_link_list = []
+top_title_list = []
 top_link_list = []
-tag_list = []
+top_vote_list = []
+top_tag_list = []
 
-# new list
-def getNewList(keyword):
-    new_list = []
+def get_new_response(keyword):
     url1 = 'https://api.stackexchange.com/2.3/questions?'
     oneDayAgo = datetime.now() - timedelta(1)
-    url2 = 'fromdate='+str(oneDayAgo.date())+'&order=desc&sort=votes&'    
+    url2 = 'fromdate='+str(oneDayAgo.date())+'&order=desc&sort=creation&'    
     url3 = 'tagged='+str(keyword)+'&site=stackoverflow'
     response = requests.get(url1 + url2 + url3).json()
-    i = 0
-    for x in range(len(response['items'])):
-        new_list.append("{}- ".format(i+1) + response['items'][x]['title'])
-        new_link_list.append(response['items'][x]['link'])
-        i += 1
-        if (i == 10):
-            break
-    return new_list
+    return response
 
-# top list
-def getTopList(keyword):
-    top_list = []
+def get_top_response(keyword):
     oneWeekAgo = datetime.now() - timedelta(7)
     url1 = 'https://api.stackexchange.com/2.3/questions?'
     url2 = 'fromdate='+str(oneWeekAgo.date())+'&order=desc&sort=votes&'    
     url3 = 'tagged='+str(keyword)+'&site=stackoverflow'
-    response = requests.get(url1 + url2 + url3).json()    
+    response = requests.get(url1 + url2 + url3).json()
+    return response
+
+def get_new_list(keyword):
+    # reset the list
+    global new_link_list, new_title_list
+    new_title_list = []
+    new_link_list = []
+    response = get_new_response(keyword)
+    # print('%%%%%%%%%%%%%%%%%%%%%%')
+    # print(response)
+    # if the response is empty, return an empty list
+    if 'error_id' in response:
+         return []
+
+    i = 0    
+    for x in range(len(response['items'])):
+        new_title_list.append("{}- ".format(i+1) + response['items'][x]['title'])
+        new_link_list.append(response['items'][x]['link'])
+        i += 1
+        if (i == 10):
+            break
+
+    return new_title_list
+
+def get_top_list(keyword):
+    global top_title_list, top_link_list, top_vote_list
+    top_title_list = []
+    top_link_list = []
+    top_vote_list = []
+
+    response = get_top_response(keyword)
+    # print('%%%%%%%%%%%%%%%%%%%%%%')
+    # print(response)
+    # if the response is empty, return an empty list
+    if 'error_id' in response:
+         return []
+
     i = 0
     for x in range(len(response['items'])):
-        top_list.append("{}- ".format(i+1) + response['items'][x]['title'])
+        top_title_list.append("{}- ".format(i+1) + response['items'][x]['title'])
         top_link_list.append(response['items'][x]['link'])
+        top_vote_list.append(response['items'][x]['score'])
         i += 1
         if (i == 10):
             break
@@ -44,19 +74,19 @@ def getTopList(keyword):
     # find all tags
     for i in range(len(response['items'])):
         for item in response['items'][i]['tags']:
-            tag_list.append(item)
+            top_tag_list.append(item)
     
-    return top_list    
+    return top_title_list    
 
-def trends(tagList):
-    cnt = Counter(tagList)
+def trends(tags):
+    cnt = Counter(tags)
     temp = cnt.most_common(5)
     top_five = []
     for item in temp:
         top_five.append('#'+str(item[0]))
     # empty tag_list
-    global tag_list
-    tag_list = []
+    global top_tag_list
+    top_tag_list = []
     return top_five
 
 def index(request):
@@ -65,38 +95,46 @@ def index(request):
     # if the input is empty, return nothing
     if keyword == '':
         return render(request, 'results/index.html')    
-        
-    class post:
+
+    class newPost:
         def __init__(self, link, id):           
             self.link = link
             self.id = id
-
-    lst1=getNewList(keyword)
-    lst2=getTopList(keyword)
-    lst3=[]
-    lst4=[]
+    lst1=get_new_list(keyword)
+    lst2=[]
     i=0
     for item in lst1:        
-        lst3.append(post(item, i))
-        i += 1
-    
-    i=0
-    for item in lst2:        
-        lst4.append(post(item, i))
+        lst2.append(newPost(item, i))
         i += 1
 
+    class topPost:
+        def __init__(self, link, id, vote):           
+            self.link = link
+            self.id = id
+            self.vote = vote
+
+    
+    lst3=get_top_list(keyword)    
+    lst4=[]
+        
+    i=0
+    for item in lst3:        
+        lst4.append(topPost(item, i, top_vote_list[i]))
+        i += 1   
+
     context = {
-        'new_list': lst3,
+        'new_list': lst2,
         'top_list': lst4,
-        'tags': trends(tag_list),
+        'tags': trends(top_tag_list),
     }
     
     return render(request, 'results/index.html', context)
 
-def NewlistItem(request, id):
+def new_list_item(request, id):
     context = {}
     link = new_link_list[id]
     r = requests.get(link)
+    # beautiful soup
     c = bs(r.content, features="html.parser")
     title = c.find('div', attrs={'id': 'question-header'}).h1.get_text()
     Question = c.find('div', attrs={'class': 'question'}).prettify()
@@ -109,10 +147,11 @@ def NewlistItem(request, id):
     }
     return render(request, 'results/listItem.html', context)
 
-def ToplistItem(request, id):
+def top_list_item(request, id):
     context = {}
     link = top_link_list[id]
     r = requests.get(link)
+    # beautiful soup
     c = bs(r.content, features="html.parser")
     title = c.find('div', attrs={'id': 'question-header'}).h1.get_text()
     Question = c.find('div', attrs={'class': 'question'}).prettify()
